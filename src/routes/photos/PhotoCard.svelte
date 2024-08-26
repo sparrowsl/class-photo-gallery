91955d37c1af14946a4a9d44db75cd5a0191074f
sparrowsl <benthorpe.dev@gmail.com>
2024-08-26 12:26:01 +0000

    feat: view image largely on modal view

--- src/routes/photos/PhotoCard.svelte
+++ src/routes/photos/PhotoCard.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import type { Photo } from "$lib/types.ts";
+	import Icon from "@iconify/svelte";
 
 	export let photo: Photo;
+
+	let modal: HTMLDialogElement;
 </script>
 
 <figure class="shadow-lg min-h-52 bg-white rounded">
@@ -19,5 +22,23 @@
 			<p class="badge badge-accent badge-sm">{photo.university}</p>
 			<p class="text-xs">{photo.classYear}</p>
 		</div>
+
+		<button class="block mt-5" on:click={() => modal.showModal()}>
+			<Icon icon="mdi:eye" class="text-xl text-info" />
+		</button>
 	</figcaption>
 </figure>
+
+<dialog bind:this={modal} class="modal">
+	<div class="modal-box">
+		<img
+			src="data:image/webp;base64,{photo.blob}"
+			alt={photo.caption}
+			class="w-full h-full"
+		/>
+		<p class="py-4 italic">Press ESC key or click outside to close</p>
+	</div>
+	<form method="dialog" class="modal-backdrop">
+		<button>close</button>
+	</form>
+</dialog>
